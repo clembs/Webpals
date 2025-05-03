f988e7707133326e0884b050b0ceee9b13e5bec1
Clément Voisin <clembs@clembs.com>
2025-05-04 00:19:06 +0200

    feat: new navbar style

--- src/lib/components/NavBar/NavBar.svelte
+++ src/lib/components/NavBar/NavBar.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import { House, Bell, BellRinging } from 'phosphor-svelte';
+	import { Bell, BellRinging, Compass } from 'phosphor-svelte';
 	import Avatar from '../Avatar.svelte';
 	import NotificationsMenu from './NotificationsMenu.svelte';
 	import { clickoutside } from '@svelte-put/clickoutside';
-	import { enhance } from '$app/forms';
 	import AccountMenu from './AccountMenu.svelte';
 	import { clientSupabase } from '$lib/db/supabase';
 	import { onDestroy, onMount } from 'svelte';
 	import { invalidate } from '$app/navigation';
 	import type { RealtimeChannel } from '@supabase/supabase-js';
 	import Webpals from '$icons/Webpals.svelte';
+	import Button from '../Button.svelte';
 
 	let accountMenuOpen = $state(false);
 	let notificationsMenuOpen = $state(false);
@@ -42,134 +42,148 @@
 </script>
 
 <header>
+	<a class="icon-button" href="/" aria-label="Home" title="Home">
+		<Webpals size="2rem" />
+	</a>
+
 	<nav>
-		<a class="logo" href="/" aria-label="Home" title="Home">
-			<Webpals />
-		</a>
-
-		<div class="right">
-			<ul>
-				<li>
-					<a href="/" aria-label="Feeds" title="Feeds" aria-current={page.url.pathname === '/'}>
-						<House weight={page.url.pathname === '/' ? 'fill' : 'regular'} />
-					</a>
-				</li>
-				<!-- <li>
-					<button aria-label="Theme settings" title="Theme settings">
-						<Palette weight="regular" />
-					</button>
-				</li> -->
-				{#if page.data.currentProfile}
-					<li
-						data-submenu="true"
-						use:clickoutside
-						onclickoutside={() => (notificationsMenuOpen = false)}
+		<ul>
+			<li>
+				<a
+					class="icon-button"
+					href="/explore"
+					aria-label="Explore"
+					title="Explore"
+					aria-current={page.url.pathname === '/explore'}
+				>
+					<Compass weight={page.url.pathname === '/explore' ? 'fill' : 'regular'} />
+				</a>
+			</li>
+			{#if page.data.currentProfile}
+				<li
+					data-has-submenu="true"
+					use:clickoutside
+					onclickoutside={() => (notificationsMenuOpen = false)}
+				>
+					<button
+						class="icon-button"
+						onclick={() => (notificationsMenuOpen = !notificationsMenuOpen)}
+						aria-current={notificationsMenuOpen}
+						aria-label="Notifications"
+						title="Notifications"
 					>
-						<form use:enhance action="/api/notifications?/markAllAsRead" method="post">
-							<button
-								onclick={() => (notificationsMenuOpen = !notificationsMenuOpen)}
-								aria-current={notificationsMenuOpen}
-								aria-label="Notifications"
-								title="Notifications"
-							>
-								{#if unreadNotifications!.length > 0}
-									<div aria-label="{unreadNotifications!.length} new notifications" class="badge">
-										{unreadNotifications!.length}
-									</div>
-									<BellRinging weight={notificationsMenuOpen ? 'fill' : 'regular'} />
-								{:else}
-									<Bell weight={notificationsMenuOpen ? 'fill' : 'regular'} />
-								{/if}
-							</button>
-						</form>
-
-						{#if notificationsMenuOpen}
-							<NotificationsMenu
-								bind:menuOpen={notificationsMenuOpen}
-								user={page.data.currentProfile}
-							/>
+						{#if unreadNotifications!.length > 0}
+							<div aria-label="{unreadNotifications!.length} new notifications" class="badge">
+								{unreadNotifications!.length}
+							</div>
+							<BellRinging weight={notificationsMenuOpen ? 'fill' : 'regular'} />
+						{:else}
+							<Bell weight={notificationsMenuOpen ? 'fill' : 'regular'} />
 						{/if}
-					</li>
-				{/if}
-			</ul>
-			{#if !page.data.currentProfile}
-				<a href="/login"> Join Webpals </a>
-			{:else}
-				<div
+					</button>
+
+					{#if notificationsMenuOpen}
+						<NotificationsMenu
+							bind:menuOpen={notificationsMenuOpen}
+							user={page.data.currentProfile}
+						/>
+					{/if}
+				</li>
+			{/if}
+
+			{#if page.data.currentProfile}
+				<li
+					id="account-menu-wrapper"
 					use:clickoutside
 					onclickoutside={() => (accountMenuOpen = false)}
-					class="account-menu-wrapper"
+					data-has-submenu="true"
 				>
 					<button
 						use:clickoutside
 						onclick={() => (accountMenuOpen = !accountMenuOpen)}
 						aria-label="Profile"
 						title="Profile"
+						class="icon-button"
 					>
-						<Avatar user={page.data.currentProfile} size="40px" />
+						<Avatar user={page.data.currentProfile} size="2.5rem" />
 					</button>
+
 					{#if accountMenuOpen}
 						<AccountMenu bind:menuOpen={accountMenuOpen} user={page.data.currentProfile} />
 					{/if}
-				</div>
+				</li>
+			{:else}
+				<li id="join-cta-wrapper">
+					<Button href="/login" size="sm">Join Webpals</Button>
+				</li>
 			{/if}
-		</div>
+		</ul>
 	</nav>
 </header>
 
 <style lang="scss">
 	:root {
-		--navbar-height: calc(40px + var(--base-padding) * 1.5);
+		// Size of all navigation buttons within the navbar
+		--navbar-button-size: 3rem;
+		--navbar-padding: calc(var(--base-padding) * 0.5);
+		// Calculated height of the navbar (including margins), used globally
+		--navbar-height: calc(
+			var(--navbar-button-size) + var(--navbar-padding) * 2 + var(--inputs-border-width) * 2 +
+				var(--base-padding) * 2
+		);
 	}
 
-	nav {
+	header {
+		// Layout
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		padding: calc(var(--base-padding) * 0.25) var(--base-padding);
+
+		// Gaps, paddings, margins
+		width: calc(100% - var(--base-padding) * 2);
+		max-width: 60rem;
+		margin: var(--base-padding) auto;
+		padding: var(--navbar-padding);
 		gap: var(--base-gap);
+
+		// Visual flair
 		background-color: var(--widgets-background-color);
-		height: var(--navbar-height);
+		border: var(--widgets-border-width) solid var(--widgets-border-color);
+		border-radius: calc(var(--widgets-border-base-radius) * 2);
+		box-shadow: var(--widgets-box-shadow-x) var(--widgets-box-shadow-y)
+			var(--widgets-box-shadow-blur) var(--widgets-box-shadow-spread)
+			var(--widgets-box-shadow-color);
+	}
 
-		a,
-		button {
-			display: grid;
-			place-items: center;
-			border-radius: 99px;
-			padding: calc(var(--base-padding) * 0.5);
-			background-color: transparent;
-			color: var(--color-heading);
-			cursor: pointer;
-			border: none;
-			position: relative;
+	ul {
+		display: flex;
+		align-items: center;
 
-			&[aria-current='true'] {
-				background-color: var(--widgets-background-color-dim);
-			}
+		list-style: none;
+	}
 
-			&:global(.logo svg) {
-				width: 2rem;
-				height: 2rem;
-			}
+	li[data-has-submenu='true'] {
+		position: relative;
+	}
 
-			&:hover {
-				backdrop-filter: brightness(0.95);
-			}
+	.icon-button {
+		display: grid;
+		place-items: center;
+		border-radius: 99px;
+		height: var(--navbar-button-size);
+		width: var(--navbar-button-size);
+		background-color: transparent;
+		color: var(--color-heading);
+		cursor: pointer;
+		border: none;
+		position: relative;
+
+		&[aria-current='true'] {
+			background-color: var(--widgets-background-color-dim);
 		}
 
-		.right {
-			display: flex;
-			gap: calc(var(--base-gap) * 0.5);
-			align-items: center;
-		}
-
-		ul {
-			display: contents;
-			list-style: none;
-
-			li[data-submenu='true'] {
-				position: relative;
-			}
+		&:hover {
+			backdrop-filter: brightness(0.95);
 		}
 
 		.badge {
@@ -186,8 +200,13 @@
 			width: calc(var(--base-padding) * 1.25);
 			font-size: 14px;
 		}
-		.account-menu-wrapper {
-			position: relative;
-		}
+	}
+
+	#account-menu-wrapper {
+		margin-left: 0.25rem;
+	}
+
+	#join-cta-wrapper {
+		margin: 0.5rem;
 	}
 </style>
