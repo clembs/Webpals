b743d2aa72ed9d3611a8d680990ce757916b8238
Clembs <clembs@clembs.com>
2025-05-10 14:36:27 +0200

    refactor(widgets/about-me): inline editing

--- src/lib/widgets/about-me/AboutMeWidget.svelte
+++ src/lib/widgets/about-me/AboutMeWidget.svelte
@@ -1,59 +1,42 @@
 <script lang="ts">
 	import BaseWidget from '$lib/widgets/BaseWidget.svelte';
-	import Button from '$lib/components/Button.svelte';
 	import type { AboutMeJSON, WidgetComponentProps } from '../types';
 	import { enhance } from '$app/forms';
 	import { parseMarkdown } from '$lib/helpers/text';
+	import InlineTextInput from '$lib/components/InlineTextInput.svelte';
 
 	let { widget, isEditing }: WidgetComponentProps<AboutMeJSON> = $props();
+
+	let formEl = $state<HTMLFormElement>();
+	let contentValue = $state(widget.content);
 </script>
 
 <BaseWidget {widget} {isEditing}>
-	{#snippet settingsDialog()}
+	<h3>About me</h3>
+	{#if isEditing}
 		<form
-			use:enhance={() =>
-				({ update }) => {
-					update({ reset: false });
-				}}
-			class="about-me-edit"
+			use:enhance={() => {
+				if (contentValue === widget.content) return;
+			}}
 			action="/api/profile?/editAboutMe"
 			method="post"
+			bind:this={formEl}
 		>
-			<h2>About me</h2>
-			<textarea name="content" value={widget.content}></textarea>
-			<Button type="submit">Save</Button>
+			<InlineTextInput
+				fullWidth
+				name="content"
+				bind:value={contentValue}
+				multiline
+				onblur={() => formEl?.requestSubmit()}
+			/>
 		</form>
-	{/snippet}
-	<div class="about-me">
-		<h2>About me</h2>
+	{:else}
 		{@html parseMarkdown(widget.content)}
-	</div>
+	{/if}
 </BaseWidget>
 
 <style lang="scss">
-	.about-me {
-		display: flex;
-		flex-direction: column;
-		gap: var(--base-gap);
-	}
-	h2 {
-		font-size: 1.5rem;
-	}
-
-	.about-me-edit {
-		display: flex;
-		flex-direction: column;
-		gap: var(--base-gap);
-
-		textarea {
-			background-color: var(--widgets-background-color);
-			color: var(--color-paragraph);
-			border: none;
-			outline: none;
-			font-size: 0.9rem;
-			transition: font-size 200ms;
-			resize: vertical;
-			min-height: 10rem;
-		}
+	form {
+		display: contents;
 	}
 </style>
