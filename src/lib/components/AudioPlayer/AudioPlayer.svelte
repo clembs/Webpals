586def4bc092448db84c13891e3b1330bd79075d
Clembs <clembs@clembs.com>
2025-05-12 01:54:47 +0200

    refactor(components/audioplayer): use buttons and change range input style

--- src/lib/components/AudioPlayer/AudioPlayer.svelte
+++ src/lib/components/AudioPlayer/AudioPlayer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Pause, Play, SpeakerHigh, SpeakerNone } from 'phosphor-svelte';
 	import { AudioPlayer } from './headless.svelte';
+	import Button from '../Button.svelte';
 
 	let {
 		src,
@@ -22,21 +23,17 @@
 
 <div class="audio-player">
 	<div class="buttons">
-		<button type="button" onclick={() => audioPlayer?.togglePlayback()} class="toggle-playback">
-			{#if paused}
-				<Play />
-			{:else}
-				<Pause />
-			{/if}
-		</button>
+		<Button
+			onclick={() => audioPlayer?.togglePlayback()}
+			icon={paused ? Play : Pause}
+			variant="text"
+		/>
 
-		<button type="button" onclick={() => audioPlayer?.toggleMute()} class="mute">
-			{#if muted}
-				<SpeakerNone />
-			{:else}
-				<SpeakerHigh />
-			{/if}
-		</button>
+		<Button
+			onclick={() => audioPlayer?.toggleMute()}
+			icon={muted ? SpeakerNone : SpeakerHigh}
+			variant="text"
+		/>
 	</div>
 
 	<input
@@ -51,90 +48,74 @@
 		type="range"
 		name="audio-track"
 	/>
-</div>
 
-<audio
-	bind:this={audioEl}
-	controls
-	bind:paused
-	bind:muted
-	bind:currentTime
-	bind:duration
-	ontimeupdate={() => (rangeValue = (currentTime / duration) * 100)}
->
-	<source {src} />
-	Your browser does not support the audio element.
-</audio>
+	<audio
+		bind:this={audioEl}
+		controls
+		bind:paused
+		bind:muted
+		bind:currentTime
+		bind:duration
+		ontimeupdate={() => (rangeValue = (currentTime / duration) * 100)}
+	>
+		<source {src} />
+		Your browser does not support the audio element.
+	</audio>
+</div>
 
 <style lang="scss">
 	.audio-player {
-		background-color: var(--widgets-background-color-dim);
-		padding: calc(var(--base-padding) * 0.25);
-		// padding-left: calc(var(--base-padding) * 0.25);
-		padding-right: calc(var(--base-padding) * 0.75);
-		border-radius: var(--inputs-border-base-radius);
-		border: var(--inputs-border-width) solid var(--inputs-border-color);
-
-		width: 100%;
 		display: flex;
 		align-items: center;
-		gap: var(--base-gap);
+
+		width: 100%;
+		gap: calc(var(--base-gap) * 0.5);
+		padding-top: calc(var(--base-padding) * 0.25);
+		padding-bottom: calc(var(--base-padding) * 0.25);
+		padding-left: calc(var(--base-padding) * 0.25);
+		padding-right: var(--base-padding);
+
+		background-color: var(--widgets-background-color-dim);
+		border-radius: var(--inputs-border-base-radius);
 
 		.buttons {
 			display: flex;
-			gap: calc(var(--base-gap) * 0.5);
-		}
-
-		button {
-			padding: calc(var(--base-padding) * 0.5);
-			border-radius: var(--inputs-border-base-radius);
-			border: none;
-			cursor: pointer;
-			background-color: transparent;
-			color: inherit;
 		}
 
 		[name='audio-track'] {
 			--track-height: calc(var(--base-padding) * 0.25);
 			--thumb-size: var(--base-padding);
 
-			cursor: pointer;
-			flex-grow: 1;
+			height: var(--track-height);
+
 			background-color: var(--widgets-border-color);
 			border-radius: 99px;
-			height: var(--track-height);
-			accent-color: black;
+			// we forget webkit/chrome because it can't style the progress
+			// like on firefox
+			// as a workaround the closest thing we have is accent-color
+			// (not guaranteed to look consistent between implementations though)
+			//
+			// in the future i'd use something more custom than an input[type='range'] lol
+			accent-color: var(--buttons-primary-background-color);
 
-			&::-webkit-slider-thumb {
-				appearance: none;
-				height: var(--thumb-size);
-				width: var(--thumb-size);
-				border-radius: 99px;
-				background-color: var(--widgets-background-color);
-				border: 3px solid var(--buttons-primary-background-color);
-				margin-top: calc(var(--track-height) * 0.5 - max(var(--thumb-size) * 0.5, 3px));
-			}
-			&::-webkit-slider-runnable-track {
-				// background-color: var(--buttons-primary-background-color);
-				border: none;
-				height: var(--track-height);
-				border-radius: 99px;
-			}
+			flex: 1;
+			cursor: pointer;
+
 			&::-moz-range-thumb {
-				background-color: var(--widgets-background-color);
-				border: 3px solid var(--buttons-primary-background-color);
+				border: none;
+				background-color: var(--buttons-primary-background-color);
 				border-radius: 99px;
 			}
 			&::-moz-range-progress {
-				background-color: var(--buttons-primary-background-color);
-				border: none;
 				height: calc(var(--track-height) * 1.25);
+
+				border: none;
+				background-color: var(--buttons-primary-background-color);
 				border-radius: 99px;
 			}
 		}
 	}
 
-	// TODO: don't forget to remove this
 	audio {
 		display: none;
 	}
